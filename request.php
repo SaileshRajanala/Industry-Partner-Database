b8b47191f349f07b31d41c04dd327fcf2d5b02ca
Sailesh Rajanala <sailesh777@live.com>
2020-11-03 21:28:33 -0600

    updates

--- request.php
+++ request.php
@@ -1,34 +1,35 @@
 <!DOCTYPE html>
   <head>
-    <style>
-          div {
-            margin: auto;
-            width: 50%;
-            padding: 10px;
-          }
-          table, th, td {
-          border: 3px solid red;
-          border-collapse: collapse;
-          padding:5px;
-          }
-    </style>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width">
+    <title>Industry Partner Database</title>
+    <!-- <link href="style.css" rel="stylesheet" type="text/css"> -->
+
+    <link href="style_dark.css" id="fS" rel="stylesheet" type="text/css">
+
+    <!-- Font -->
+    <link href="https://fonts.googleapis.com/css2?family=Quicksand&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Comfortaa&display=swap" rel="stylesheet">
   </head>
   <body>
   </body>
 <?php
 include "connect.php" ;
 
-$sql = "SELECT First_Name, Last_Name, phoneNo,college,currentStatus,linkedin,workplace,position,notes FROM primary_table";
+$sql = "SELECT First_Name, Last_Name, phoneNo,college,currentStatus,linkedin,workplace,position,notes FROM Contacts";
 $result = $conn->query($sql);
 
-if ($result->num_rows > 0) {
-
+if ($result->num_rows > 0) 
+{
     echo "<div><table>";
     echo "<tr>" ;
     echo "<td>First Name </td> <td> Last Name </td> <td > Phone No</td> <td> College </td> <td> Current Status </td> <td>LinkedIn </td> <td> Work Place </td> <td> Position </td> <td> Notes </td>" ;
     echo "</tr>";
-    while($row = $result->fetch_assoc()) {
-      if($row["First_Name"] != ""){
+
+    while($row = $result->fetch_assoc()) 
+    {
+      if($row["First_Name"] != "")
+      {
         echo "<tr>";
          echo "<td>". $row["First_Name"]. " </td><td> " . $row["Last_Name"] . "</td><td>" . $row["phoneNo"]. "</td><td>" . $row["college"]. "</td><td>" . $row["currentStatus"]. "</td><td>" . $row["linkedin"]. "</td><td>" .  $row["workplace"] . "</td><td>" . $row["position"] . "</td><td>" . $row["notes"]. "</td>";
         echo "</tr>";
--- submit.php
+++ submit.php
@@ -1,7 +1,27 @@
+<!DOCTYPE html>
+  <head>
+       <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width">
+    <title>Industry Partner Database</title>
+    <!-- <link href="style.css" rel="stylesheet" type="text/css"> -->
+
+    <link href="style_dark.css" id="fS" rel="stylesheet" type="text/css">
+
+    <!-- Font -->
+    <link href="https://fonts.googleapis.com/css2?family=Quicksand&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Comfortaa&display=swap" rel="stylesheet">
+  
+
+  </head>
+  <body>
+  </body>
+
 <?php
 include "connect.php" ;
+$prefix = $_POST["prefix"];
 $fname = $_POST["fname"];
 $lname = $_POST["lname"];
+$email = $_POST["email"];
 $phoneno = $_POST["phoneno"];
 $college = $_POST["college"];
 $currsts = $_POST["currsts"];
@@ -10,13 +30,17 @@ $workplace = $_POST["workplace"];
 $position = $_POST["position"];
 $notes = $_POST["notes"];
 
-$sql = "INSERT INTO primary_table  VALUES('$fname','$lname','$phoneno','$college','$currsts','$linkedin','$workplace','$position','$notes')";
+$sql = "INSERT INTO Contacts (Prefix,First_Name, Last_Name, Email, Phone_Number, College, Current_Status, LinkedIn, Workplace, Title, Notes)
+VALUES('$prefix','$fname','$lname','adshisd@asd.com','$phoneno','$college','$currsts','$linkedin','$workplace','$position','$notes');";
 //SET First_Name='Alfred Schmidt', Last_Name='Frankfurt'";
 if (mysqli_query($conn, $sql)) {
-  echo "<iframe src=\"https://giphy.com/embed/el7VG1XOOvi24oRXFt\" width=\"480\" height=\"480\" frameBorder=\"0\" class=\"giphy-embed\" allowFullScreen></iframe><p><a href=\"https://giphy.com/gifs/rickandmorty-season-1-adult-swim-rick-and-morty-el7VG1XOOvi24oRXFt\"></a></p>";
+  echo "<center><h1 class=\"mainTitle\"> Thank you for <br> your entry.</h1>";
+  echo "<link href=\"style_dark.css\" id=\"fS\" rel=\"stylesheet\" type=\"text/css\">";
 } else {
   echo "Error updating record: " . mysqli_error($conn);
 }
 
 
  ?>
+
+
