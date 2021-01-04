0e759a1032874b28107fc5b6b7b2178e5a7f9287
Sailesh Rajanala <41392184+SaileshRajanala@users.noreply.github.com>
2021-01-04 15:30:16 -0600

    updates

--- exportExcel.php
+++ exportExcel.php
@@ -11,35 +11,39 @@ $conn = mysqli_connect($server, $user, $password, "id15084806_main_database");
 if (!$conn) 
   die("Connection failed: " . mysqli_connect_error());
 
-$result = $conn->query($sql);
-
-$file_ending = "xls";
-
-header("Content-Type: application/xls");    
-header("Content-Disposition: attachment; filename=$filename.xls");  
-header("Pragma: no-cache"); 
-header("Expires: 0");
-
-$flag = false;
-while ($row = $result->fetch_assoc()) 
+public function exportToExcel($result)
 {
-    if (!$flag) 
+    // $result = $conn->query($sql);
+
+    $file_ending = "xls";
+
+    header("Content-Type: application/xls");    
+    header("Content-Disposition: attachment; filename=$filename.xls");  
+    header("Pragma: no-cache"); 
+    header("Expires: 0");
+
+    $flag = false;
+
+    while ($row = $result->fetch_assoc()) 
     {
-        echo implode("\t", array_keys($row)) . "\r\n";
-        $flag = true;
+        if (!$flag) 
+        {
+            echo implode("\t", array_keys($row)) . "\r\n";
+            $flag = true;
+        }
+
+        $copy = array_values($row);
+
+        for ($i=0; $i < count($copy); $i++) 
+        { 
+            $copy[$i] = str_replace("\r\n", "", $copy[$i]);
+            $copy[$i] = str_replace("\r\t", "", $copy[$i]);
+            $copy[$i] = str_replace("\n", "", $copy[$i]);
+            $copy[$i] = str_replace("\t", "", $copy[$i]);
+        }
+
+        echo implode("\t", $copy) . "\r\n";
     }
-
-    $copy = array_values($row);
-
-    for ($i=0; $i < count($copy); $i++) 
-    { 
-        $copy[$i] = str_replace("\r\n", "", $copy[$i]);
-        $copy[$i] = str_replace("\r\t", "", $copy[$i]);
-        $copy[$i] = str_replace("\n", "", $copy[$i]);
-        $copy[$i] = str_replace("\t", "", $copy[$i]);
-    }
-
-    echo implode("\t", $copy) . "\r\n";
 }
 
 ?>
