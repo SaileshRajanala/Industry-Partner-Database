1f0f7146c531dfd0ef590143d1ca73a52108917c
Sailesh Rajanala <41392184+SaileshRajanala@users.noreply.github.com>
2021-03-17 17:54:01 -0500

    1 and 2 mixed

--- formTestScript.js
+++ formTestScript.js
@@ -53,7 +53,7 @@ for (var i = formDivs.length - 1; i >= 0; i--) {
   formDivs[i].style.display = 'none';
 }
 
-formDivs[3].style.display = 'block';
+formDivs[0].style.display = 'block';
 // INITIALIZE FORM END##########################################################################
 
 
