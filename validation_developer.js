ec042b9162e225ff7e22336a4aecbef7ac9c710e
Sailesh Rajanala <41392184+SaileshRajanala@users.noreply.github.com>
2021-03-23 01:28:25 -0500

    updates

--- validation_developer.js
+++ validation_developer.js
@@ -580,9 +580,9 @@ function switchDiv(targetDiv, currentDiv, _button = "Next")
     isFilled_childTextBox('college_education3', 'college_degree_year')                  && 
     isFilled_childTextBox('college_education4', 'college_degree_year')                  && 
     isFilled_childTextBox('college_education5', 'college_degree_year')                  &&
-    isSelected_childRadioGroup('college_education5', 'BS_school')                && 
-    isSelected_childRadioGroup('BS_wsu', 'BS_field', true)                       &&
-    isSelected_childRadioGroup('BS_other', 'BS_field', true)                     &&
+    isSelected_childRadioGroup('college_education5', 'BS_school')                       && 
+    isSelected_childRadioGroup('BS_wsu', 'BS_field', true)                              &&
+    isSelected_childRadioGroup('BS_other', 'BS_field', true)                            &&
     isFilled_childTextBox('BS_other', 'BS_other_school')                                &&
     isSelected_childCheckBoxGroup('BS_Engineering', 'BS_Engineering_Discipline', true)  &&
     isSelected_childRadioGroup('college_education5', 'haveMastersDegree')               &&
