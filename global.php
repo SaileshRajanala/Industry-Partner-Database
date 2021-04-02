edde5f69d3aecc918a836bbcdfa7e353907e976f
Sailesh Rajanala <41392184+SaileshRajanala@users.noreply.github.com>
2021-04-01 20:57:12 -0500

    Update global.php

--- global.php
+++ global.php
@@ -3,39 +3,39 @@
 include "connect.php";
 // Array to store all field ids from HTML
 
-// $htmlFields = ["prefix"];
+$htmlFields = ["prefix"];
 
-// array_push($htmlFields, "first_name");
-// array_push($htmlFields, "last_name");
-// array_push($htmlFields, "email");
-// array_push($htmlFields, "phone_number");
-// array_push($htmlFields, "college");
-// array_push($htmlFields, "current_status");
-// array_push($htmlFields, "linkedin");
-// array_push($htmlFields, "workplace");
-// array_push($htmlFields, "title");
-// array_push($htmlFields, "notes");
+array_push($htmlFields, "first_name");
+array_push($htmlFields, "last_name");
+array_push($htmlFields, "email");
+array_push($htmlFields, "phone_number");
+array_push($htmlFields, "college");
+array_push($htmlFields, "current_status");
+array_push($htmlFields, "linkedin");
+array_push($htmlFields, "workplace");
+array_push($htmlFields, "title");
+array_push($htmlFields, "notes");
 
 // Array to store all columns from SQL
 
-// $tableColumns = ["Prefix"];
+$tableColumns = ["Prefix"];
 
-// array_push($tableColumns, "First_Name");
-// array_push($tableColumns, "Last_Name");
-// array_push($tableColumns, "Email");
-// array_push($tableColumns, "Phone_Number");
-// array_push($tableColumns, "College");
-// array_push($tableColumns, "Current_Status");
-// array_push($tableColumns, "LinkedIn");
-// array_push($tableColumns, "Workplace");
-// array_push($tableColumns, "Title");
-// array_push($tableColumns, "Notes");
+array_push($tableColumns, "First_Name");
+array_push($tableColumns, "Last_Name");
+array_push($tableColumns, "Email");
+array_push($tableColumns, "Phone_Number");
+array_push($tableColumns, "College");
+array_push($tableColumns, "Current_Status");
+array_push($tableColumns, "LinkedIn");
+array_push($tableColumns, "Workplace");
+array_push($tableColumns, "Title");
+array_push($tableColumns, "Notes");
 
 // Insert Schema Automation below
-// $insertSchema = "Prefix";
+$insertSchema = "Prefix";
 
-// for ($i = 1; $i < count($tableColumns); $i++)
-//   $insertSchema .= ", " . $tableColumns[$i];
+for ($i = 1; $i < count($tableColumns); $i++)
+  $insertSchema .= ", " . $tableColumns[$i];
 
 // $valueSchema = "'{$_REQUEST["prefix"]}'";
 
