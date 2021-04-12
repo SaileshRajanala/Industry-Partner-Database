33970ec3ebe8793592175f1dc1e35bd8da9399f4
Sailesh Rajanala <41392184+SaileshRajanala@users.noreply.github.com>
2021-04-12 15:54:31 -0500

    Update connect.php

--- connect.php
+++ connect.php
@@ -2,8 +2,9 @@
 $servername = "mysprod.wichita.edu";
 $username   =     "wpaccessibility";
 $password   =     "wpaccessibility";
+$database   = 	  "wpaccessibility";
 
-$conn = mysqli_connect($servername, $username, $password, "id15084806_main_database");
+$conn = mysqli_connect($servername, $username, $password, $database);
 if (!$conn) {
   die("Connection failed: " . mysqli_connect_error());
 }
