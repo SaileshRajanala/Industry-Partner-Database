d6c0bdf0693910d0a2ff5786ff0b87ec2c45ab49
Sailesh Rajanala <41392184+SaileshRajanala@users.noreply.github.com>
2021-04-27 15:21:33 -0500

    aurora js updated

--- wallpaper.js
+++ wallpaper.js
@@ -1,17 +1,26 @@
 var d = new Date();
 
-if (d.getHours() >= 6 && d.getHours() < 18)
-{ // bright mode
-	;
-}
-else 
-{ // dark mode
-	if (d.getDay() == 1 || d.getDay() == 4 || d.getDay() == 6) 
-	{ // Mondays, Thursdays, Saturdays
-		document.body.style.backgroundImage = 'url("stars_aurora.png")';
-	}	
-	else
-	{
-		document.body.style.backgroundImage = 'url("stars.png")';
-	}
+// if (d.getHours() >= 6 && d.getHours() < 18)
+// { // bright mode
+// 	;
+// }
+// else 
+// { // dark mode
+// 	if (d.getDay() == 1 || d.getDay() == 4 || d.getDay() == 6) 
+// 	{ // Mondays, Thursdays, Saturdays
+// 		document.body.style.backgroundImage = 'url("stars_aurora.png")';
+// 	}	
+// 	else
+// 	{
+// 		document.body.style.backgroundImage = 'url("stars.png")';
+// 	}
+// }
+
+if (d.getDay() == 1 || d.getDay() == 4 || d.getDay() == 6) 
+{ // Mondays, Thursdays, Saturdays
+	document.body.style.backgroundImage = 'url("stars_aurora.png")';
+}	
+else
+{
+	document.body.style.backgroundImage = 'url("stars.png")';
 }
