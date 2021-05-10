410693c51a438573eeac2659695ddb9ed86a6280
Sailesh Rajanala <41392184+SaileshRajanala@users.noreply.github.com>
2021-05-10 17:10:21 -0500

    login css updates mobile and firefox

--- login.php
+++ login.php
@@ -18,6 +18,7 @@ if(!empty($_SESSION['user_id']))
   <link href="https://fonts.googleapis.com/css2?family=Quicksand&display=swap" rel="stylesheet">
 
   <link rel="stylesheet" href="login_style.css">
+
 </head>
 
 <body name="body">
@@ -47,4 +48,11 @@ if(!empty($_SESSION['user_id']))
 
 
 </body>
-</html>
+
+<script type="text/javascript">
+
+    document.body.style.backgroundImage = "url(\'wsu" + 
+    (Math.floor(Math.random() * 6) + 1) + ".jpg\')";
+
+</script>
+  </html>
