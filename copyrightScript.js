47817321c4a1cf813e07e2ee073bc668327ab994
Sailesh Rajanala <41392184+SaileshRajanala@users.noreply.github.com>
2021-05-12 10:27:47 -0500

    Update copyrightScript.js

--- copyrightScript.js
+++ copyrightScript.js
@@ -9,7 +9,7 @@ id_('copyrightInfo').innerHTML = 'Copyright <i class="far fa-copyright"></i>'
 							   + ' 2020-' + d.getFullYear() + '. '  
 							   + '<a href="https://saileshrajanala.github.io/teamLotus.html" '
 							   + 'class="linkB" target="_blank"' 
-							   + 'style="text-decoration:none; background-color:white;color:black;border:0.1em solid black;padding-left:2%;padding-right:2%;">'
+							   + 'style="text-decoration:none; background-color:black;color:white;padding-left:2%;padding-right:2%;">'
 							   + 'Team Lotus</a>';
 
 
